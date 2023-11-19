f444d3e5c09cd99f7da76257d90da6fa0220c39c
Elliott Barringer <elliottbarringer@icloud.org>
2023-11-19 13:24:01 -0800

    Push 2

--- classes.hpp
+++ classes.hpp
@@ -2,31 +2,41 @@
 #include <fstream>
 #include <chrono>
 #include <string>
+#include <vector>
 
 auto currentT = std::chrono::system_clock::now();
 
 struct DueDate
 {
     std::chrono::system_clock::time_point timePoint;
-    DueDate(std::chrono::system_clock::time_point tp) : timePoint(tp) {}
+    int task_no;
+    DueDate(std::chrono::system_clock::time_point tp, int task) : timePoint(tp), task_no(task) {}
 
     bool isPast() const
     {
         return std::chrono::system_clock::now() > timePoint;
     }
+
 };
 
 class Task
 {
     public:
         std::string task_txt;
-        int timefromNow;
+        int timefromNow; // in hours
+        int task_no;
         bool isCompleted;
-    Task(std::string task_txt, int timefromNow, bool isCompleted)
+    Task(std::string task_txt, int timefromNow, int task_no, bool isCompleted)
     {
         this->task_txt = task_txt;
         this->timefromNow = timefromNow;
+        this->task_no = task_no;
         this->isCompleted = isCompleted;
+    }
+    void createdueDate()
+    {
+        auto dt = currentT + std::chrono::hours(this->timefromNow);
+        DueDate dd(dt,  this->task_no)
     }
 };
 
@@ -34,15 +44,17 @@ class TaskManager
 {
     public:
         int no_tasks;
-    void createTask(std::string task_txt, int timefromNow, bool isCompleted)
+        std::vector<Task> tasksV;
+    void createTask(std::string task_txt, int timefromNow, int task_no, bool isCompleted)
     {
-        Task t(task_txt, timefromNow, isCompleted);
+        Task t(task_txt, timefromNow, task_no, isCompleted);
+        this->tasksV.emplace_back(t);
         this->no_tasks += 1;
 
         std::ofstream t_file("tasks.txt");
         t_file << this->no_tasks << std::endl << t.task_txt;
     }
-    std::string getT(int t_no)
+    std::string getT(int t_no) //intake task number as arg
     {
         char target = t_no;
         std::string filename = "tasks.txt";
@@ -65,5 +77,5 @@ class TaskManager
         }
         inputFile.close();
         return "";
-    }
+    } 
 };
